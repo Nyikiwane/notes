15c473e1dfe9fc731b8094bd64f70824bc4fe6f2
Nelson Manhisse <nmanhisse@gmail.com>
2023-08-27 02:02:18 +0900

    layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,17 +4,38 @@ import styles from './Layout.module.css';
 //   children: React.ReactNode;
 // }
 
+const notes = [
+  {
+    title: 'Title 1',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus enim, architecto debitis maxime reprehenderit porro natus repellat quam mollitia dignissimos quis magnam neque ullam ab adipisci voluptate deserunt nisi! Veritatis laborum repellat obcaecati, maiores sunt ipsum. Maiores deleniti alias odio autem similique accusamus, libero reiciendis unde aut optio fuga illo, modi laborum fugit quidem. Culpa quos non neque iste?',
+  },
+  {
+    title: 'Title 2',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus enim, architecto debitis maxime reprehenderit porro natus repellat quam mollitia dignissimos quis magnam neque ullam ab adipisci voluptate deserunt nisi! Veritatis laborum repellat obcaecati, maiores sunt ipsum. Maiores deleniti alias odio autem similique accusamus, libero reiciendis unde aut optio fuga illo, modi laborum fugit quidem. Culpa quos non neque iste?',
+  },
+  {
+    title: 'Title 3',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus enim, architecto debitis maxime reprehenderit porro natus repellat quam mollitia dignissimos quis magnam neque ullam ab adipisci voluptate deserunt nisi! Veritatis laborum repellat obcaecati, maiores sunt ipsum. Maiores deleniti alias odio autem similique accusamus, libero reiciendis unde aut optio fuga illo, modi laborum fugit quidem. Culpa quos non neque iste?',
+  },
+  {
+    title: 'Title 4',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus enim, architecto debitis maxime reprehenderit porro natus repellat quam mollitia dignissimos quis magnam neque ullam ab adipisci voluptate deserunt nisi! Veritatis laborum repellat obcaecati, maiores sunt ipsum. Maiores deleniti alias odio autem similique accusamus, libero reiciendis unde aut optio fuga illo, modi laborum fugit quidem. Culpa quos non neque iste?',
+  },
+];
+
 function Layout() {
   return (
     <div className={styles.layout}>
       <div className={styles.menu}>
-        <ul>
-          <li>Menu</li>
-          <li>Menu</li>
-          <li>Menu</li>
-          <li>Menu</li>
-          <li>Menu</li>
-        </ul>
+          <ul className={styles['menu-list']}>
+            {notes.map((n) => (
+              <li className={styles['menu-note']} key={n.title}>
+                <a className={styles['menu-note__note']} href='/'>
+                  <span>{n.title}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
       </div>
       <div className={styles.content}>Content</div>
     </div>
