df5e10cdc8e156a0630dc206eb8423e9948285d8
Nelson Manhisse <nmanhisse@gmail.com>
2023-09-03 01:53:27 +0900

    sort by date - newest

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -6,31 +6,33 @@ import { makeSlugFromTitle } from '../helpers/helpers';
 
 function NotesList() {
   const [path, setPath] = useState('/');
-  const pathname = useLocation()?.pathname; // to strip the /
+  const pathname = useLocation().pathname; 
   useEffect(() => {
     if (pathname !== '/') {
-      setPath(pathname.slice(1));
+      setPath(pathname.slice(1)); // to strip the '/'
     }
   }, [pathname]);
 
   return (
     <ul className={styles['menu-list']}>
-      {notes.notes.map((note) => {
-        const slug = makeSlugFromTitle(note.title);
-        return (
-          <li
-            className={`${styles['menu-note']} ${
-              path === slug ? styles['menu-note__active--note'] : ''
-            }`}
-            key={slug}
-          >
-            <Link to={slug} className={styles['menu-note__note']}>
-              <span className={styles['menu-note__title']}>{note.title}</span>
-              <span className={styles['menu-note__body']}>{note.body}</span>
-            </Link>
-          </li>
-        );
-      })}
+      {notes.notes
+        .sort((a, b) => (new Date(a.date) > new Date(b.date) ? -1 : 1))
+        .map((note) => {
+          const slug = makeSlugFromTitle(note.title);
+          return (
+            <li
+              className={`${styles['menu-note']} ${
+                path === slug ? styles['menu-note__active--note'] : ''
+              }`}
+              key={slug}
+            >
+              <Link to={slug} className={styles['menu-note__note']}>
+                <span className={styles['menu-note__title']}>{note.title}</span>
+                <span className={styles['menu-note__body']}>{note.body}</span>
+              </Link>
+            </li>
+          );
+        })}
     </ul>
   );
 }
