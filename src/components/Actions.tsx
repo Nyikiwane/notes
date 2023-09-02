b744a83f15dcec174fddfba898d129e8b5570b7c
Nelson Manhisse <nmanhisse@gmail.com>
2023-09-02 19:16:43 +0900

    added drawer for notes menu

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Actions.module.css';
 import FormatBoldIcon from '@mui/icons-material/FormatBold';
 import EditNoteIcon from '@mui/icons-material/EditNote';
@@ -8,32 +8,52 @@ import NoteAddIcon from '@mui/icons-material/NoteAdd';
 import SaveIcon from '@mui/icons-material/Save';
 import TitleIcon from '@mui/icons-material/Title';
 import NotesIcon from '@mui/icons-material/Notes';
+import Drawer from '@mui/material/Drawer';
+import { useState } from 'react';
 
 function Actions() {
   const pathname = useLocation().pathname;
+  const [openNotesMenu, setOpenNotesMenu] = useState(false);
+
+  const openNotesMenuHandler = () => setOpenNotesMenu(true);
+  const closeNotesMenuHandler = () => setOpenNotesMenu(false);
 
   return (
-    <div className={styles.actions}>
-      <div className={styles['actions__notes-list']}>
-        <NotesIcon />
-      </div>
-      {pathname !== '/' && (
-        <div className={styles['actions__write--del']}>
-          <NoteAddIcon sx={{ pr: 2 }} />
-          <EditNoteIcon sx={{ pr: 2 }} />
-          <DeleteOutlineIcon />
+    <>
+      <div className={styles.actions}>
+        <div
+          className={styles['actions__notes-list']}
+          onClick={openNotesMenuHandler}
+        >
+          <NotesIcon />
         </div>
-      )}
+        {pathname !== '/' && (
+          <div className={styles['actions__write--del']}>
+            <Link to='/' className={styles['actions__new-note']}>
+              <NoteAddIcon sx={{ pr: 2 }} />
+            </Link>
+            <EditNoteIcon sx={{ pr: 2 }} />
+            <DeleteOutlineIcon />
+          </div>
+        )}
 
-      {pathname === '/' && (
-        <div className={styles['actions__format']}>
-          <SaveIcon/>
-          <FormatBoldIcon sx={{ pl: 2 }} />
-          <FormatItalicIcon sx={{ pl: 2 }} />
-          <TitleIcon sx={{ pl: 2 }} />
-        </div>
-      )}
-    </div>
+        {pathname === '/' && (
+          <div className={styles['actions__format']}>
+            <SaveIcon />
+            <FormatBoldIcon sx={{ pl: 2 }} />
+            <FormatItalicIcon sx={{ pl: 2 }} />
+            <TitleIcon sx={{ pl: 2 }} />
+          </div>
+        )}
+      </div>
+      <Drawer
+        sx={{ width: '50%' }}
+        open={openNotesMenu}
+        onClose={closeNotesMenuHandler}
+      >
+        <div> this noteeeeeeee</div>
+      </Drawer>
+    </>
   );
 }
 
--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,6 +1,11 @@
 import { useParams } from 'react-router-dom';
 import notes from '../lib/notes.json';
 
+interface Note {
+  title: string;
+  body: string;
+}
+
 function Note() {
   const { slug } = useParams();
   const note = notes[slug];
