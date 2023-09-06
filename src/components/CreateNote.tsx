9e4623c41743692a76b9c267275d66cb8f2a3e07
Nelson Manhisse <nmanhisse@gmail.com>
2023-09-06 19:48:02 +0900

    changed TextField to a div & edit handlers

--- src/components/CreateNote.tsx
+++ src/components/CreateNote.tsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
-import './CreateNote.module.css';
+import styles from './CreateNote.module.css';
 import Box from '@mui/material/Box';
 import { Event } from '../vite-env';
 import notes from '../lib/notes.json';
-import TextField from '@mui/material/TextField';
 import Actions from './Actions';
 import { useNavigate } from 'react-router-dom';
 import { makeSlugFromTitle } from '../helpers/helpers';
 
 function CreateNote() {
   const [note, setNote] = useState('');
+  const [selectedText, setSlectedText] = useState('');
   const navigation = useNavigate();
 
-  const onChangeHandler = (e: Event) => {
-    setNote(e.target.value);
+  const onChangeHandler = (e: React.FormEvent<HTMLDivElement>) => {
+    setNote(e.currentTarget.textContent as string);
   };
 
   const onSubmitHandler = (e: Event | React.MouseEvent) => {
@@ -27,24 +27,48 @@ function CreateNote() {
     navigation(`/${makeSlugFromTitle(title)}`);
   };
 
+  const selectTextHandler = () => {
+    const t = window.getSelection()?.toString();
+    if (t) {
+      setSlectedText(t);
+    }
+  };
+
+  const boldTextHandler = () => {
+    if (selectedText && note.includes(selectedText)) {
+      const boldT = `<b>${selectedText}</b>`;
+      setNote(note.replace(selectedText, boldT));
+    }
+  };
+
+  const italicizeTextHandler = () => {
+    if (selectedText && note.includes(selectedText)) {
+      const italicT = `<i>${selectedText}</i>`;
+      setNote(note.replace(selectedText, italicT));
+    }
+  };
+
+  const headerTextHandler = () => {
+    if (selectedText && note.includes(selectedText)) {
+      const headerT = `<h1>${selectedText}</h1>`;
+      setNote(note.replace(selectedText, headerT));
+    }
+  };
+
   return (
     <Box component='form'>
       <Actions
         onSave={onSubmitHandler}
-        onBold={() => console.log('bold')}
-        onItalic={() => console.log('bold')}
-        onHeading={() => console.log('bold')}
+        onBold={boldTextHandler}
+        onItalic={italicizeTextHandler}
+        onHeading={headerTextHandler}
       />
-      <TextField
-        multiline
-        fullWidth
-        placeholder='New note'
-        onChange={onChangeHandler}
-        sx={{
-          '& fieldset': {
-            border: 'none',
-          },
-        }}
+      <div
+        className={styles.textarea}
+        contentEditable
+        dangerouslySetInnerHTML={{ __html: `${note ?? ''}` }}
+        onSelect={selectTextHandler}
+        onInput={onChangeHandler}
       />
     </Box>
   );
